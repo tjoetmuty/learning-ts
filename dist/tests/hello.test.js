3bb9d2f9ca9ef88b0dea7706fce01a4fc8b104d7
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-02-09 21:01:05 +0800

    compile typescript include and exclude

---
+++ dist/tests/hello.test.js
@@ -0,0 +1,7 @@
+"use strict";
+describe('hello', () => {
+    it("should say hello", () => {
+        const name = 'hello muty';
+        expect(name).toBe('hello muty');
+    });
+});
---
+++ dist/tests/say-hello.test.js
@@ -0,0 +1,6 @@
+import { sayHello } from "../src/say-hello";
+describe('sayHello', () => {
+    it('should return hello muty', () => {
+        expect(sayHello('muty')).toBe('hello muty');
+    });
+});
