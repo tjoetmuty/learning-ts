d736024054966352dda045d3b7c0c0cf8b7f1046
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-02-09 21:14:37 +0800

    data type of typescript

---
+++ dist/tests/tipe-data.test.js
@@ -0,0 +1,11 @@
+"use strict";
+describe('Data type', () => {
+    it('shhould must declare', () => {
+        const name = "Tjoet Muty Ahmad";
+        const balance = 100;
+        const isTrue = true;
+        console.log(name);
+        console.log(balance);
+        console.log(isTrue);
+    });
+});
---
+++ tests/tipe-data.test.ts
@@ -0,0 +1,11 @@
+describe('Data type', () => {
+    it('shhould must declare', () => {
+        const name :string = "Tjoet Muty Ahmad"
+        const balance : number = 100
+        const isTrue: boolean = true
+
+        console.log(name)
+        console.log(balance)
+        console.log(isTrue)
+    })
+})
