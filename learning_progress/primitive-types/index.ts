c5e05dffbe0ccf0216fa7fd4811665e6a8786807
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-10-01 14:25:47 +0700

    primitive types: string number and boolean

---
+++ learning_progress/primitive-types/index.ts
@@ -0,0 +1,20 @@
+//data type
+
+//1. string
+const halo : string = "muty"
+const age = 3
+const ageAsString: string = age.toString()
+
+console.log(halo)
+console.log(age)
+console.log(ageAsString)
+
+//2. number
+const umur: number = 20
+const stringToNumber: number = parseInt("2004")
+console.log(stringToNumber)
+
+//3. boolean
+const isGirl: boolean = true
+const minimumAge = age <= 3 ? true : false
+console.log(minimumAge)
